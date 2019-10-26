0d65f0b1042253b85e2c9a9fcb4ccabdaea4c163
MattJaccino <36944187+MattJaccino@users.noreply.github.com>
2019-10-26 12:30:12 -0400

    Update word_freq_ranker.py

--- word_freq_ranker.py
+++ word_freq_ranker.py
@@ -3,11 +3,14 @@ from operator import itemgetter
 
 	
 def word_clean(word):
+    # Remove any punctuation from words after split()
     alpha = "abcdefghijklmnopqrstuvwxyz'"
     i = 0
     word = word.lower()
     while i < len(word):
+        # Slice out anything not in our alphabet
         if word[i] not in alpha:
+            # If at the end, just remove the last char
             if i == len(word) - 1:
                 word = word[:i]
             else:
@@ -29,22 +32,27 @@ def rank(filename):
             else:
                 word_dict[word] = 1
 
+    # This will be the list of all words and their count in dict
     word_list = []
     for word in word_dict:
         word_list.append((word, word_dict[word]))
+        # Sort the list by the count in descending order
         word_list.sort(key=itemgetter(1), reverse=True)
 		
+    # Creating the output text file
     wff_name = "Word Frequencies for '" + filename[:-4] + "'.txt"
     ranked_word_file = open(wff_name, "w")
     for item in word_list:
         entry = item[0] + "  :  " + str(item[1]) + '\n'
         ranked_word_file.write(entry)
 
+    # Print the top 3 to console
     print("Top 3 Most Frequent Words:")
     for i in range(1, 4):
         entry = word_list[i-1][0] + "  :  " + str(word_list[i-1][1])
         print(i, ". ", entry, sep='')
 
+    # Close file streams
     word_file.close()
     ranked_word_file.close()
 	
